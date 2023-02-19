3acd165677196de9d47e4f384fe78a8e76de5aad
VeryHighMan <ipun0013@gmail.com>
2023-02-19 13:18:30 +0900

    first commit

---
+++ src/main/java/com/defr/hotdealradar/HotdealradarApplication.java
@@ -0,0 +1,13 @@
+package com.defr.hotdealradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdealradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotdealradarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/defr/hotdealradar/HotdealradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defr.hotdealradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdealradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
