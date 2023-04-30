3aeca73d98026633cc495db7ba72a5c97c713749
VeryHighMan <ipun0013@gmail.com>
2023-05-01 08:51:09 +0900

    WIP

--- src/main/java/com/defr/hotdealradar/batch/ScheduleTask.java
+++ src/main/java/com/defr/hotdealradar/batch/ScheduleTask.java
@@ -1,6 +1,33 @@
 package com.defr.hotdealradar.batch;
 
+
+import com.defr.hotdealradar.common.StoredValue;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
 public class ScheduleTask {
+
+
+
+    @Scheduled(cron = "0/10 * * * * *")
+    private void makeHotDealData() {
+        System.out.println("핫딜 데이터 제작");
+        System.out.println("메모리 핫딜 데이터 : " + StoredValue.hotDealData);
+
+        //사이트별 크롤링 & 정제 클래스
+
+        StoredValue.hotDealData.put("pomppu", new Object());
+
+
+
+
+
+
+    }
+
 
 
 
---
+++ src/main/java/com/defr/hotdealradar/common/StoredValue.java
@@ -0,0 +1,10 @@
+package com.defr.hotdealradar.common;
+
+import java.util.HashMap;
+
+public class StoredValue {
+
+    public static HashMap<String, Object> hotDealData = new HashMap<String, Object>();
+
+
+}
